9a3709cab9702b16e08375150dc6536d949a9758
Praveen <123054620+praveen0723@users.noreply.github.com>
2023-07-07 15:54:48 +0530

    aura unrealted to lwc

---
+++ force-app/main/default/aura/lmsAuraComponent/lmsAuraComponentController.js
@@ -0,0 +1,19 @@
+({
+    handleMessage : function(component, message) {
+     if(message != null&& message.getParam("lmsData") !=null){
+        component.set("v.messageRecieved", message.getParam("lmsData").value)
+     }
+    },
+    inputHandler : function(component, event){
+        component.set("v.messageValue",event.target.value)
+    },
+    publishMessage:function(component){
+        let msg =component.get("v.messageValue")
+        let message={
+            lmsData:{
+              value:msg
+            }
+        }
+        component.find("SampleMessageChannel").publish(message)
+    }
+})
